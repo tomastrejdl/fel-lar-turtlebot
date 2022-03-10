04992de18962848d4722ca904c39379266949f7b
Tomáš Trejdl <tom.trejdl@seznam.cz>
2022-03-10 13:31:59 +0100

    Add find_green_gate

--- gradual_rollout.py
+++ gradual_rollout.py
@@ -1,9 +1,11 @@
 from robolab_turtlebot import Turtlebot, Rate, get_time
+import math
 
 turtle = Turtlebot()
+# turtle = Turtlebot(rgb=True, depth=True, pc=True)
 rate = Rate(10)
 
-isRunning = False
+isRunning = True
 
 # Names bumpers and events
 bumper_names = ['LEFT', 'CENTER', 'RIGHT']
@@ -14,10 +16,17 @@ def goForward():
 
 def forceStop():
     print('FORCE STOPPING')
-    sRunning = False
+    global isRunning
+    isRunning = False
     turtle.cmd_velocity(linear = 0)
     turtle.cmd_velocity(angular = 0)
     quit()
+
+def rotateLeft():
+    turtle.cmd_velocity(angular = 0.4)
+
+def rotateRight():
+    turtle.cmd_velocity(angular = -0.4)
 
 def bumper_cb(msg):
     """Bumber callback."""
@@ -33,19 +42,31 @@ def bumper_cb(msg):
         print('Bumper pressed Stopping')
         forceStop()
 
+def drive1m():
+    t = get_time()
+    while not turtle.is_shutting_down() and isRunning and get_time() - t < 10:
+        goForward()
+        print(turtle.get_odometry())
+        rate.sleep()
+
+def rotate180():
+    currentAngle = turtle.get_odometry()[2]
+    while not turtle.is_shutting_down() and isRunning and currentAngle > -0.1 and  currentAngle <= math.pi :
+        rotateLeft()
+        print(turtle.get_odometry())
+        rate.sleep()
+        currentAngle = turtle.get_odometry()[2]
+
 def main():
+    print('Starting...')
     turtle.register_bumper_event_cb(bumper_cb)
     turtle.reset_odometry()
 
-    isRunning = True
-
-    t = get_time()
-
-    while not turtle.is_shutting_down():
-        while get_time() - t < 10:
-            goForward()
-            # print(turtle.get_odometry())
-            rate.sleep()
+    while not turtle.is_shutting_down() and isRunning:
+        drive1m()
+        rotate180()
+    
+    turtle.cmd_velocity(linear = 0)
 
 
 if __name__ == '__main__':
