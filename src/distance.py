af26b763955102adc1a98d4fd34762385e3d36d9
sidddd8 <100859713+sidddd8@users.noreply.github.com>
2022-04-14 14:36:25 +0200

    Update distance.py

--- src/distance.py
+++ src/distance.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
+
 from robolab_turtlebot import Turtlebot
 import numpy as np
 import cv2
-
-from constants import *
 
 def euclid_norm(x, y, z):
     return np.sqrt(x*x + y*y + z*z)
@@ -13,7 +12,41 @@ def pc_to_distance(pc, row, column):
     x = pc[row][column][0]
     y = pc[row][column][1]
     z = pc[row][column][2]
-    return euclid_norm(x, y, z)
+    return x, y, z, euclid_norm(x, y, z)
+
+def get_coords(pc, row, column):
+    x = pc[row][column][0]
+    y = pc[row][column][1]
+    z = pc[row][column][2]
+    return x, y, z
+
+def get_intersections(x0, y0, r0, x1, y1, r1):
+    # circle 1: (x0, y0), radius r0
+    # circle 2: (x1, y1), radius r1
+
+    d=math.sqrt((x1-x0)**2 + (y1-y0)**2)
+
+    # non intersecting
+    if d > r0 + r1 :
+        return None
+    # One circle within other
+    if d < abs(r0-r1):
+        return None
+    # coincident circles
+    if d == 0 and r0 == r1:
+        return None
+    else:
+        a=(r0**2-r1**2+d**2)/(2*d)
+        h=math.sqrt(r0**2-a**2)
+        x2=x0+a*(x1-x0)/d   
+        y2=y0+a*(y1-y0)/d   
+        x3=x2+h*(y1-y0)/d     
+        y3=y2-h*(x1-x0)/d 
+
+        x4=x2-h*(y1-y0)/d
+        y4=y2+h*(x1-x0)/d
+        
+        return (x3, y3, x4, y4)
 
 def find_closest_gate_color():
     # TODO: Implement this
