d6eef982f43c6461f036b7dd5d76be7b290f31e1
Robin4242 <100533911+Robin4242@users.noreply.github.com>
2022-04-28 16:12:47 +0200

    Add files via upload

---
+++ src/transform_coords.py
@@ -0,0 +1,8 @@
+import math
+
+def transform_coords(x,y,rotation_angle_in_deg):
+    distance = math.sqrt(x**2+y**2)
+    pole_angle = math.radians(rotation_angle_in_deg) - math.atan2(x,y)
+    real_x_coords = (pole_angle/abs(pole_angle))* distance * math.sin(abs(pole_angle))
+    real_y_coords = distance * math.cos(abs(pole_angle))
+    return real_x_coords, real_y_coords
