b27adb2f4180fadf19736b629c88484e7226a3de
Robin4242 <100533911+Robin4242@users.noreply.github.com>
2022-05-10 12:39:27 +0200

    Přidaná podmínka != 0

--- src/transform_coords.py
+++ src/transform_coords.py
@@ -3,8 +3,9 @@ import math
 def transform_coords(x, y, rotation_angle_in_deg):
     distance = math.sqrt(x**2 + y**2)
     pole_angle = math.radians(rotation_angle_in_deg) - math.atan2(x, y)
-    real_x_coords = (
-        -(pole_angle / abs(pole_angle)) * distance * math.sin(abs(pole_angle))
-    )
+    if pole_angle != 0:
+        real_x_coords = (-(pole_angle / abs(pole_angle)) * distance * math.sin(abs(pole_angle)))
+    else:
+        real_x_coords = 0
     real_y_coords = distance * math.cos(abs(pole_angle))
     return real_x_coords, real_y_coords
